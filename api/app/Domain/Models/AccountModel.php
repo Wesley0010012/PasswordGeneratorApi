5a7a8e230324e18e725ba027c566f6547548ed30
Wesley0010012 <wglaurindo33@gmail.com>
2024-05-19 16:14:37 -0300

    refactor: add missed id in account model

--- api/app/Domain/Models/AccountModel.php
+++ api/app/Domain/Models/AccountModel.php
@@ -4,9 +4,20 @@ namespace App\Domain\Models;
 
 class AccountModel
 {
+    private int $id;
     private string $name;
     private string $email;
     private string $password;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
 
     public function getName(): string
     {
