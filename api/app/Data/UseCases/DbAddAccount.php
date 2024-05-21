9d1e8f70bd4594ce8a2702ae008dde3d66fdc395
Wesley0010012 <wglaurindo33@gmail.com>
2024-05-21 19:19:41 -0300

    feat: ensure DbAddAccount throw if Encrypter throws

--- api/app/Data/UseCases/DbAddAccount.php
+++ api/app/Data/UseCases/DbAddAccount.php
@@ -3,18 +3,23 @@
 namespace App\Data\UseCases;
 
 use App\Data\Protocols\AddAccountRepository;
+use App\Data\Protocols\Encrypter;
 use App\Domain\Models\AddAccountModel;
 use App\Domain\Models\AccountModel;
 use App\Domain\UseCases\AddAccount;
 
 class DbAddAccount implements AddAccount
 {
-    public function __construct(private AddAccountRepository $addAccountRepository)
-    {
+    public function __construct(
+        private AddAccountRepository $addAccountRepository,
+        private Encrypter $encrypter
+    ) {
     }
 
     public function add(AddAccountModel $addAccountModel): AccountModel
     {
+        $addAccountModel->setPassword($this->encrypter->encrypt($addAccountModel->getPassword()));
+
         $id = $this->addAccountRepository->add($addAccountModel);
 
         $accountModel = new AccountModel();
