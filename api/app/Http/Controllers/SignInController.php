68fbb03a798f966980498533ae2f8785d06dc72a
Wesley0010012 <wglaurindo33@gmail.com>
2024-06-29 16:24:10 -0300

    feat: ensure return 400 if invalid email was provided

--- api/app/Http/Controllers/SignInController.php
+++ api/app/Http/Controllers/SignInController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\InvalidParamError;
 use App\Exceptions\MissingParamError;
 use App\Http\Helpers\HttpHelpers;
+use App\Http\Protocols\EmailValidator;
 use App\Http\Protocols\HttpRequest;
 use App\Http\Protocols\HttpResponse;
 
 class SignInController extends Controller
 {
+    public function __construct(private readonly EmailValidator $emailValidator)
+    {
+    }
+
     public function handle(HttpRequest $httpRequest): HttpResponse
     {
         $data = $httpRequest->getBody();
@@ -19,6 +25,12 @@ class SignInController extends Controller
             if (!$data[$param]) {
                 return HttpHelpers::badRequest(new MissingParamError($param));
             }
+        }
+
+        ['email' => $email, 'password' => $password] = $data;
+
+        if (!$this->emailValidator->validate($email)) {
+            return HttpHelpers::badRequest(new InvalidParamError('email'));
         }
 
         return HttpHelpers::success("ANY_SUCCESS");
