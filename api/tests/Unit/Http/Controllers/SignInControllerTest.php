24e9166afc4490541efa42e6e7bfaeabc1b9c5b7
Wesley0010012 <wglaurindo33@gmail.com>
2024-06-29 16:19:03 -0300

    test: ensure SignInController return 400 if no email was provided

---
+++ api/tests/Unit/Http/Controllers/SignInControllerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Unit\Http\Controllers;
+
+use App\Exceptions\MissingParamError;
+use App\Http\Controllers\SignInController;
+use App\Http\Protocols\HttpRequest;
+use Tests\TestCase;
+
+class SignInControllerTest extends TestCase
+{
+    private SignInController $sut;
+
+    public function setUp(): void
+    {
+        $this->sut = new SignInController();
+    }
+
+    public function testEnsureCorrectInstance()
+    {
+        $this->assertInstanceOf(SignInController::class, $this->sut);
+    }
+
+    public function testShouldReturn400IfNoEmailWasProvided()
+    {
+        $httpRequest = new HttpRequest();
+        $httpRequest->setBody([
+            'email' => '',
+            'password' => 'any_password'
+        ]);
+
+        $httpResponse = $this->sut->handle($httpRequest);
+
+        $error = new MissingParamError('email');
+
+        $this->assertEquals(400, $httpResponse->getStatusCode());
+        $this->assertInstanceOf(MissingParamError::class, $httpResponse->getBody());
+        $this->assertEquals($error->getMessage(), $httpResponse->getBody()->getMessage());
+    }
+}
