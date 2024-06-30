b6aa772f64ad4dd84f7b1388a3ada6a8cb63abe6
Wesley0010012 <wglaurindo33@gmail.com>
2024-06-30 11:35:11 -0300

    feat: ensure throw if FindAccountRepository throws

--- api/app/Data/UseCases/DbFindAccount.php
+++ api/app/Data/UseCases/DbFindAccount.php
@@ -3,6 +3,7 @@
 namespace App\Data\UseCases;
 
 use App\Data\Protocols\Encrypter;
+use App\Data\Protocols\FindAccountRepository;
 use App\Domain\Models\FindAccountModel;
 use App\Domain\Models\AccountModel;
 use App\Domain\UseCases\FindAccount;
@@ -10,7 +11,8 @@ use App\Domain\UseCases\FindAccount;
 class DbFindAccount implements FindAccount
 {
     public function __construct(
-        private readonly Encrypter $encrypter
+        private readonly Encrypter $encrypter,
+        private readonly FindAccountRepository $findAccountRepository
     ) {
     }
 
@@ -18,6 +20,6 @@ class DbFindAccount implements FindAccount
     {
         $findAccountModel->setPassword($this->encrypter->encrypt($findAccountModel->getPassword()));
 
-        return null;
+        return $this->findAccountRepository->findAccountData($findAccountModel);
     }
 }
