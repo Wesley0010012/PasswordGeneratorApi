8eec953d2947c354b1da5c79bcee7d10d0ae29f4
Wesley0010012 <wglaurindo33@gmail.com>
2024-06-30 10:45:33 -0300

    feat: ensure return 500 if EmailValidator throws

--- api/app/Http/Controllers/SignInController.php
+++ api/app/Http/Controllers/SignInController.php
@@ -8,6 +8,7 @@ use App\Http\Helpers\HttpHelpers;
 use App\Http\Protocols\EmailValidator;
 use App\Http\Protocols\HttpRequest;
 use App\Http\Protocols\HttpResponse;
+use Throwable;
 
 class SignInController extends Controller
 {
@@ -17,22 +18,26 @@ class SignInController extends Controller
 
     public function handle(HttpRequest $httpRequest): HttpResponse
     {
-        $data = $httpRequest->getBody();
+        try {
+            $data = $httpRequest->getBody();
 
-        $params = ['email', 'password'];
+            $params = ['email', 'password'];
 
-        foreach ($params as $param) {
-            if (!$data[$param]) {
-                return HttpHelpers::badRequest(new MissingParamError($param));
+            foreach ($params as $param) {
+                if (!$data[$param]) {
+                    return HttpHelpers::badRequest(new MissingParamError($param));
+                }
             }
+
+            ['email' => $email, 'password' => $password] = $data;
+
+            if (!$this->emailValidator->validate($email)) {
+                return HttpHelpers::badRequest(new InvalidParamError('email'));
+            }
+
+            return HttpHelpers::success("ANY_SUCCESS");
+        } catch (Throwable $e) {
+            return HttpHelpers::internalServerError();
         }
-
-        ['email' => $email, 'password' => $password] = $data;
-
-        if (!$this->emailValidator->validate($email)) {
-            return HttpHelpers::badRequest(new InvalidParamError('email'));
-        }
-
-        return HttpHelpers::success("ANY_SUCCESS");
     }
 }
