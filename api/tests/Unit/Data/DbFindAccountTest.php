ae40b6ad8f61406e9517a0bd13a0d327cacb6dd6
Wesley0010012 <wglaurindo33@gmail.com>
2024-06-30 11:28:57 -0300

    test: ensure DbFindAccount throws if Encrypter throws

---
+++ api/tests/Unit/Data/DbFindAccountTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Unit\Data\UseCases;
+
+use App\Data\Protocols\Encrypter;
+use App\Data\UseCases\DbFindAccount;
+use App\Domain\Models\FindAccountModel;
+use Error;
+use Tests\TestCase;
+
+class DbFindAccountTest extends TestCase
+{
+    private DbFindAccount $sut;
+
+    private Encrypter $encrypterStub;
+
+    public function setUp(): void
+    {
+        $this->encrypterStub = $this->createMock(Encrypter::class);
+
+        $this->sut = new DbFindAccount(
+            $this->encrypterStub
+        );
+    }
+
+    private function mockFindAccountModel()
+    {
+        return new FindAccountModel(
+            'valid_email',
+            'valid_password'
+        );
+    }
+
+    public function testEnsureCorrectInstance()
+    {
+        $this->assertInstanceOf(DbFindAccount::class, $this->sut);
+    }
+
+    public function testShouldThrowIfEncrypterThrows()
+    {
+        $this->expectException(Error::class);
+
+        $this->encrypterStub->method('encrypt')
+            ->willThrowException(new Error());
+
+        $data = $this->mockFindAccountModel();
+
+        $this->sut->getAccount($data);
+    }
+}
