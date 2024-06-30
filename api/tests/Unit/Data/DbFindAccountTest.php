bd20c64553efe7ffbcde3a2320ae5f807c3bb459
Wesley0010012 <wglaurindo33@gmail.com>
2024-06-30 11:35:20 -0300

    test: ensure throw if FindAccountRepository throws

--- api/tests/Unit/Data/DbFindAccountTest.php
+++ api/tests/Unit/Data/DbFindAccountTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Data\UseCases;
 
 use App\Data\Protocols\Encrypter;
+use App\Data\Protocols\FindAccountRepository;
 use App\Data\UseCases\DbFindAccount;
 use App\Domain\Models\FindAccountModel;
 use Error;
@@ -13,13 +14,16 @@ class DbFindAccountTest extends TestCase
     private DbFindAccount $sut;
 
     private Encrypter $encrypterStub;
+    private FindAccountRepository $findAccountRepositoryStub;
 
     public function setUp(): void
     {
         $this->encrypterStub = $this->createMock(Encrypter::class);
+        $this->findAccountRepositoryStub = $this->createMock(FindAccountRepository::class);
 
         $this->sut = new DbFindAccount(
-            $this->encrypterStub
+            $this->encrypterStub,
+            $this->findAccountRepositoryStub
         );
     }
 
@@ -29,6 +33,11 @@ class DbFindAccountTest extends TestCase
             'valid_email',
             'valid_password'
         );
+    }
+
+    private function mockCypheredPassword()
+    {
+        return 'cyphered_password';
     }
 
     public function testEnsureCorrectInstance()
@@ -55,6 +64,21 @@ class DbFindAccountTest extends TestCase
         $this->encrypterStub->expects($this->once())
             ->method('encrypt')
             ->with($data->getPassword());
+
+        $this->sut->getAccount($data);
+    }
+
+    public function testShouldThrowIfFindAccountRepositoryThrows()
+    {
+        $this->expectException(Error::class);
+
+        $this->encrypterStub->method('encrypt')
+            ->willReturn($this->mockCypheredPassword());
+
+        $this->findAccountRepositoryStub->method('findAccountData')
+            ->willThrowException(new Error());
+
+        $data = $this->mockFindAccountModel();
 
         $this->sut->getAccount($data);
     }
