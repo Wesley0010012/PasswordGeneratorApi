cc0a611b42bd47df2c2b1f86227015ce909a11d4
Wesley0010012 <wglaurindo33@gmail.com>
2024-07-03 20:24:43 -0300

    test: ensure GeneratePasswordController return 400 if invalid token was provided

--- api/tests/Unit/Http/Controllers/GeneratePasswordControllerTest.php
+++ api/tests/Unit/Http/Controllers/GeneratePasswordControllerTest.php
@@ -6,15 +6,21 @@ use App\Exceptions\InvalidParamError;
 use App\Exceptions\MissingParamError;
 use App\Http\Controllers\GeneratePasswordController;
 use App\Http\Protocols\HttpRequest;
+use App\Http\Protocols\TokenDecrypter;
 use Tests\TestCase;
 
 class GeneratePasswordControllerTest extends TestCase
 {
     private GeneratePasswordController $sut;
+    private TokenDecrypter $tokenDecrypterStub;
 
     public function setUp(): void
     {
-        $this->sut = new GeneratePasswordController();
+        $this->tokenDecrypterStub = $this->createMock(TokenDecrypter::class);
+
+        $this->sut = new GeneratePasswordController(
+            $this->tokenDecrypterStub
+        );
     }
 
     public function testEnsureCorrectInstance()
@@ -68,6 +74,26 @@ class GeneratePasswordControllerTest extends TestCase
         $httpResponse = $this->sut->handle($httpRequest);
 
         $error = new InvalidParamError('size');
+
+        $this->assertEquals(400, $httpResponse->getStatusCode());
+        $this->assertInstanceOf(InvalidParamError::class, $httpResponse->getBody());
+        $this->assertEquals($error->getMessage(), $httpResponse->getBody()->getMessage());
+    }
+
+    public function testShouldReturn400IfInvalidPasswordTokenWasProvided()
+    {
+        $this->tokenDecrypterStub->method('decrypt')
+            ->willReturn(false);
+
+        $httpRequest = new HttpRequest();
+        $httpRequest->setBody([
+            'token' => 'any_token',
+            'size' => 1
+        ]);
+
+        $httpResponse = $this->sut->handle($httpRequest);
+
+        $error = new InvalidParamError('token');
 
         $this->assertEquals(400, $httpResponse->getStatusCode());
         $this->assertInstanceOf(InvalidParamError::class, $httpResponse->getBody());
