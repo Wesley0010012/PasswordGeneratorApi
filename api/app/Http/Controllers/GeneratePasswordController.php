9b33e6922f57dca6cb36a59be8e1cb929faa31aa
Wesley0010012 <wglaurindo33@gmail.com>
2024-07-03 20:30:29 -0300

    feat: ensure GeneratePasswordController return 500 if TokenDecrypter throws

--- api/app/Http/Controllers/GeneratePasswordController.php
+++ api/app/Http/Controllers/GeneratePasswordController.php
@@ -8,6 +8,7 @@ use App\Http\Helpers\HttpHelpers;
 use App\Http\Protocols\HttpRequest;
 use App\Http\Protocols\HttpResponse;
 use App\Http\Protocols\TokenDecrypter;
+use Throwable;
 
 class GeneratePasswordController extends Controller
 {
@@ -17,28 +18,32 @@ class GeneratePasswordController extends Controller
 
     public function handle(HttpRequest $httpRequest): HttpResponse
     {
-        $body = $httpRequest->getBody();
+        try {
+            $body = $httpRequest->getBody();
 
-        $params = ['token', 'size'];
+            $params = ['token', 'size'];
 
-        foreach ($params as $param) {
-            if (!$body[$param]) {
-                return HttpHelpers::badRequest(new MissingParamError($param));
+            foreach ($params as $param) {
+                if (!$body[$param]) {
+                    return HttpHelpers::badRequest(new MissingParamError($param));
+                }
             }
+
+            ['token' => $token, 'size' => $size] = $body;
+
+            if ($size < 1) {
+                return HttpHelpers::badRequest(new InvalidParamError('size'));
+            }
+
+            $decryptedToken = $this->tokenDecrypter->decrypt($token);
+
+            if (!$decryptedToken) {
+                return HttpHelpers::badRequest(new InvalidParamError('token'));
+            }
+
+            return HttpHelpers::success("PASS");
+        } catch (Throwable $e) {
+            return HttpHelpers::internalServerError();
         }
-
-        ['token' => $token, 'size' => $size] = $body;
-
-        if ($size < 1) {
-            return HttpHelpers::badRequest(new InvalidParamError('size'));
-        }
-
-        $decryptedToken = $this->tokenDecrypter->decrypt($token);
-
-        if (!$decryptedToken) {
-            return HttpHelpers::badRequest(new InvalidParamError('token'));
-        }
-
-        return HttpHelpers::success("PASS");
     }
 }
