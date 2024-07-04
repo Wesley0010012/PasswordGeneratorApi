b239113e2c14a547466bff68de3911f410620d82
Wesley0010012 <wglaurindo33@gmail.com>
2024-07-04 07:02:33 -0300

    test: ensure GeneratePasswordController return 400 if TokenAccount was not finded

--- api/tests/Unit/Http/Controllers/GeneratePasswordControllerTest.php
+++ api/tests/Unit/Http/Controllers/GeneratePasswordControllerTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Unit\Http\Controllers;
 
+use App\Domain\UseCases\FindAccount;
 use App\Exceptions\InternalServerError;
 use App\Exceptions\InvalidParamError;
 use App\Exceptions\MissingParamError;
+use App\Exceptions\UnauthenticatedError;
+use App\Exceptions\UnauthorizedError;
 use App\Http\Controllers\GeneratePasswordController;
 use App\Http\Protocols\HttpRequest;
 use App\Http\Protocols\TokenDecrypter;
@@ -15,14 +18,25 @@ class GeneratePasswordControllerTest extends TestCase
 {
     private GeneratePasswordController $sut;
     private TokenDecrypter $tokenDecrypterStub;
+    private FindAccount $findAccountStub;
 
     public function setUp(): void
     {
         $this->tokenDecrypterStub = $this->createMock(TokenDecrypter::class);
+        $this->findAccountStub = $this->createMock(FindAccount::class);
 
         $this->sut = new GeneratePasswordController(
-            $this->tokenDecrypterStub
+            $this->tokenDecrypterStub,
+            $this->findAccountStub
         );
+    }
+
+    private function mockTokenAccount()
+    {
+        return [
+            'email' => 'any_email',
+            'password' => 'any_password'
+        ];
     }
 
     public function testEnsureCorrectInstance()
@@ -134,6 +148,29 @@ class GeneratePasswordControllerTest extends TestCase
 
         $this->assertEquals(500, $httpResponse->getStatusCode());
         $this->assertInstanceOf(InternalServerError::class, $httpResponse->getBody());
+        $this->assertEquals($error->getMessage(), $httpResponse->getBody()->getMessage());
+    }
+
+    public function testShouldReturn400IfTokenAccountWasNotFinded()
+    {
+        $this->tokenDecrypterStub->method('decrypt')
+            ->willReturn($this->mockTokenAccount());
+
+        $this->findAccountStub->method('getAccount')
+            ->willReturn(null);
+
+        $httpRequest = new HttpRequest();
+        $httpRequest->setBody([
+            'token' => 'any_token',
+            'size' => 1
+        ]);
+
+        $httpResponse = $this->sut->handle($httpRequest);
+
+        $error = new UnauthorizedError($httpRequest->getBody()['token']);
+
+        $this->assertEquals(400, $httpResponse->getStatusCode());
+        $this->assertInstanceOf(UnauthorizedError::class, $httpResponse->getBody());
         $this->assertEquals($error->getMessage(), $httpResponse->getBody()->getMessage());
     }
 }
