e3a781d02901d30030f2ed8df34f9993d2bd5c01
Wesley0010012 <wglaurindo33@gmail.com>
2024-07-12 07:05:55 -0300

    feat: ensure SavePasswordController return 400 if invalid token was provided

--- api/app/Http/Controllers/SavePasswordController.php
+++ api/app/Http/Controllers/SavePasswordController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Exceptions\MissingParamError;
+use App\Exceptions\UnauthorizedError;
 use App\Http\Helpers\HttpHelpers;
 use App\Http\Protocols\HttpRequest;
 use App\Http\Protocols\HttpResponse;
+use App\Http\Protocols\TokenDecrypter;
 
 class SavePasswordController extends Controller
 {
+    public function __construct(
+        private readonly TokenDecrypter $tokenDecrypter
+    ) {
+    }
+
     public function handle(HttpRequest $httpRequest): HttpResponse
     {
         $body = $httpRequest->getBody();
@@ -19,6 +26,19 @@ class SavePasswordController extends Controller
             if (!$body[$param]) {
                 return HttpHelpers::badRequest(new MissingParamError($param));
             }
+        }
+
+        [
+            'token' => $token,
+            'email' => $email,
+            'password' => $password,
+            'domain' => $domain
+        ] = $body;
+
+        $decryptedToken = $this->tokenDecrypter->decrypt($token);
+
+        if (!$decryptedToken) {
+            return HttpHelpers::badRequest(new UnauthorizedError($token));
         }
 
         return HttpHelpers::success('success');
