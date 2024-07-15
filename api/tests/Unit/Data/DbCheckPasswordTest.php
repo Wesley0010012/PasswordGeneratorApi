ce6df6a990b37c674485d93d3c820390cbe32f24
Wesley0010012 <wglaurindo33@gmail.com>
2024-07-15 06:59:56 -0300

    test: ensure DbCheckPassword throw if CheckPasswordRepository throws

---
+++ api/tests/Unit/Data/DbCheckPasswordTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Unit\Data;
+
+use App\Data\Protocols\CheckPasswordRepository;
+use App\Data\UseCases\DbCheckPassword;
+use App\Domain\Models\FindPasswordModel;
+use Error;
+use Tests\TestCase;
+
+class DbCheckPasswordTest extends TestCase
+{
+    private DbCheckPassword $sut;
+
+    private CheckPasswordRepository $checkPasswordRepositoryStub;
+
+    public function setUp(): void
+    {
+        $this->checkPasswordRepositoryStub = $this->createMock(CheckPasswordRepository::class);
+
+        $this->sut = new DbCheckPassword(
+            $this->checkPasswordRepositoryStub
+        );
+    }
+
+    private function mockFindPasswordModel()
+    {
+        return new FindPasswordModel(
+            1,
+            'any_account',
+            'any_domain'
+        );
+    }
+
+    public function testEnsureCorrectInstance()
+    {
+        $this->assertInstanceOf(DbCheckPassword::class, $this->sut);
+    }
+
+    public function testShouldThrowIfCheckPasswordRepositoryThrows()
+    {
+        $this->checkPasswordRepositoryStub->method('findPasswordByModel')
+            ->willThrowException(new Error());
+
+        $this->expectException(Error::class);
+
+        $this->sut->check($this->mockFindPasswordModel());
+    }
+}
