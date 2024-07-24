d08b93aee7269be4b44dcea7b9b7acb755bc670c
Wesley0010012 <wglaurindo33@gmail.com>
2024-07-24 18:21:24 -0300

    feat: ensure DbAddPassword throws if AddPasswordRepository throws

--- api/app/Data/UseCases/DbAddPassword.php
+++ api/app/Data/UseCases/DbAddPassword.php
@@ -2,6 +2,7 @@
 
 namespace App\Data\UseCases;
 
+use App\Data\Protocols\AddPasswordRepository;
 use App\Data\Protocols\Encrypter;
 use App\Domain\Models\AddPasswordModel;
 use App\Domain\Models\PasswordModel;
@@ -10,7 +11,8 @@ use App\Domain\UseCases\AddPassword;
 class DbAddPassword implements AddPassword
 {
     public function __construct(
-        private readonly Encrypter $encrypter
+        private readonly Encrypter $encrypter,
+        private readonly AddPasswordRepository $addPasswordRepository
     ) {
     }
 
@@ -18,6 +20,14 @@ class DbAddPassword implements AddPassword
     {
         $addPasswordModel->setPassword($this->encrypter->encrypt($addPasswordModel->getPassword()));
 
-        return new PasswordModel();
+        $passwordModel = new PasswordModel();
+        $passwordModel->setPasswordAccount($addPasswordModel->getPasswordAccount());
+        $passwordModel->setPassword($addPasswordModel->getPassword());
+        $passwordModel->setDomain($addPasswordModel->getDomain());
+        $passwordModel->setAccountId($addPasswordModel->getAccountId());
+
+        $passwordModel->setId($this->addPasswordRepository->add($passwordModel));
+
+        return $passwordModel;
     }
 }
